51667a9a8d34173cf24df08fbd95ee1011936d74
Aadarsh Thakur <aadarshthakur80501@gmail.com>
2022-07-08 10:19:52 +0530

    props

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,10 @@
-import '/Home.css'
-
+import './Home.css'
+import Product from '../Product/Product'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+        <img src="https://m.media-amazon.com/images/I/712py4WAJtL._SX750_.jpg" alt="banner" className="home__img" />
+    </div>
   )
 }
 
